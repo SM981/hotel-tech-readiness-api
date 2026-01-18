c579fa06e6ec53b3a817dcbcefcde13769ea4c76
SM981 <stewartmoss@me.com>
2026-01-18 19:03:09 +0000

    Create benchmarks.py

---
+++ benchmarks.py
@@ -0,0 +1,5 @@
+PEER_BENCHMARKS = {
+    "lifestyle_boutique": {"typical_range": (55, 65), "best_in_class": 70},
+    "independent_luxury": {"typical_range": (50, 60), "best_in_class": 65},
+    "regional_chain": {"typical_range": (45, 55), "best_in_class": 60},
+}
